2d12a86fa45c709d5666b30cf36ee0aef5e0e0a6
Darlon Vasata <darlonv@gmail.com>
2025-05-29 15:51:55 -0300

    arquivo inicial

---
+++ aulas/AulaModelo.java
@@ -0,0 +1,47 @@
+//Data: __/__/__
+//Conteúdo: _____
+
+package aulas;
+import utils.IO;
+
+public class AulaModelo {
+
+
+    public static void aula(){
+        IO.imprimir("## Aula 00 - Modelo de arquivo para aulas ##");
+    
+        //Exercício 01
+        ex01();
+
+    }
+
+    /**
+    * Título do exercício
+    * Descrição do exercício
+    */
+    public static void ex01(){
+        //Variáveis
+        
+        //Entrada
+        //Leitura dos valores
+
+        //Processamento
+        //Cálculos realizados
+        processar();
+
+        //Saída
+        //Apresentação dos resultados   
+    }
+
+    /**
+    * Descrição do método/função
+    * @param param_1 primeiro parâmetro
+    * @param param_2 segundo parâmetro
+    * @return valor retornado
+    */
+    public static int processar(int param_1, int param_2){
+        
+        return 0;
+    }
+
+}
