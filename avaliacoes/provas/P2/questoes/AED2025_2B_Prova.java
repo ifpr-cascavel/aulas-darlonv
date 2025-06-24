89e67df60af96aa6b1038ed23cedd3d7c2afcaa7
Darlon Vasata <darlonv@gmail.com>
2025-06-23 22:53:43 -0300

    prova

---
+++ avaliacoes/provas/P2/questoes/AED2025_2B_Prova.java
@@ -0,0 +1,41 @@
+package questoes;
+
+public class AED2025_2B_Prova {
+
+    public static int somar(int a, int b) {
+        // Variáveis
+        int soma;
+
+        // Processamento
+        soma = a + b;
+
+        // Retorno do resultado calculado
+        return soma;
+    }
+
+    public static int calcularPotencia(int b, int e) {
+        // TODO implementar
+        return 0;
+    }
+
+    public static int somarMultiplosIntervalo(int x, int a, int b) {
+        // TODO implementar
+        return 0;
+    }
+
+    public static int inverterDigitos(int n) {
+        // TODO implementar
+        return 0;
+    }
+
+    public static int somarPrimosIntervalo(int a, int b) {
+        // TODO implementar
+        
+        return 0;
+    }
+
+    public static int obterEnesimoPrimo(int n) {
+        // TODO implementar
+        return 0;
+    }
+}
