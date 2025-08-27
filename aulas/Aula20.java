1ca4c68e5332417beebb494cbfb6d6a67e1b73d9
Darlon Vasata <darlonv@gmail.com>
2025-08-27 20:23:01 -0300

    mudando posição no arquivo com seek

--- aulas/Aula20.java
+++ aulas/Aula20.java
@@ -7,6 +7,7 @@ import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 import utils.IO;
 
@@ -17,7 +18,8 @@ public class Aula20 {
         IO.imprimir("## Aula 20 - Arquivo binário ##");
     
         //Exercício 01
-        ex01();
+        // ex01();
+        ex02();
 
     }
 
@@ -43,7 +45,7 @@ public class Aula20 {
             file_writer = new FileOutputStream(nome_arquivo);
             writer = new DataOutputStream(file_writer);
 
-            writer.writeInt(42);
+            writer.writeInt(Integer.MIN_VALUE);
             writer.writeDouble(3.14);
             writer.writeUTF("teste");
 
@@ -54,22 +56,78 @@ public class Aula20 {
         }
 
         // leitura
+        // try {
+        //     file_reader = new FileInputStream(nome_arquivo);
+        //     reader = new DataInputStream(file_reader);
+
+        //     int valor_int = reader.readInt();
+        //     double valor_double = reader.readDouble();
+        //     String valor_string = reader.readUTF();
+        //     IO.imprimir(""+valor_int + " " + valor_double + " " + valor_string);
+
+        //     reader.close();
+        //     file_reader.close();
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+    }
+
+    public static void ex02(){
+        // Variáveis
+        RandomAccessFile file = null;
+        String nome_arquivo = "dados.bin";
+        int primeiro_valor, segundo_valor;
+
+        try{
+            file = new RandomAccessFile(nome_arquivo, "rw");
+        }catch(IOException e){
+            IO.imprimirErro("Erro durante abertura ao arquivo " + nome_arquivo + ".");
+            e.printStackTrace();
+        }
+
         try {
-            file_reader = new FileInputStream(nome_arquivo);
-            reader = new DataInputStream(file_reader);
+            // grava dois valores inteiros no arquivo
+            file.writeInt(100);
+            file.writeInt(200);
 
-            int valor_int = reader.readInt();
-            double valor_double = reader.readDouble();
-            String valor_string = reader.readUTF();
-            IO.imprimir(""+valor_int + " " + valor_double + " " + valor_string);
+            // movimenta o cursor do arquivo para a posição 0 (início do arquivo)
+            file.seek(0);
+            primeiro_valor = file.readInt(); // lê o primeiro valor do arquivo
 
-            reader.close();
-            file_reader.close();
-        } catch (IOException e) {
+            file.seek(4); // movimenta para o segundo inteiro (4 bytes)
+            segundo_valor = file.readInt();
+
+            IO.imprimir("Primeiro valor: " + primeiro_valor);
+            IO.imprimir("Segundo valor : " + segundo_valor);
+
+            file.seek(0); // movimenta novamente para a posição 0
+
+            // sobrescreve o primeiro valor
+            file.writeInt(300);
+            IO.imprimir("Sobrescrevendo..");
+
+            // Lê os valores do arquivo
+            file.seek(0); // movimenta novamente para a posição 0
+            primeiro_valor = file.readInt();
+            segundo_valor = file.readInt();
+
+            IO.imprimir("Primeiro valor: " + primeiro_valor);
+            IO.imprimir("Segundo valor : " + segundo_valor);
+
+            file.close();
+
+        }catch(IOException e){
+            IO.imprimirErro("Erro durante o acesso ao arquivo + " + nome_arquivo + ".");
+            e.printStackTrace();
+        }
+
+        try{
+            file.close();
+        }catch(IOException e){
+            IO.imprimirErro("Erro ao fechar o arquivo " + nome_arquivo + ".");
             e.printStackTrace();
         }
     }
-
     
 
 }
